46bc2d523b35f55bfdbff730c638bfdf5f22159b
Minsoo-Lee <minsoo.james1101@gmail.com>
2023-07-25 12:19:53 +0900

    Fix : add Constructors

--- erp/src/main/java/jmcompany/erp/domain/ItemInfo.java
+++ erp/src/main/java/jmcompany/erp/domain/ItemInfo.java
@@ -1,13 +1,19 @@
 package jmcompany.erp.domain;
 
 import jakarta.persistence.Embeddable;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
 import lombok.Getter;
 
 @Embeddable
 @Getter
 public class ItemInfo {
+
     private String item_num;
     private String item_name;
+
+    public ItemInfo() { }
+
+    public ItemInfo(String item_num, String item_name) {
+        this.item_num = item_num;
+        this.item_name = item_name;
+    }
 }
